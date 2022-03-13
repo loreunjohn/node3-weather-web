28723a02f6adf411861ed52cf4bb1062bd2fb619
loreunjohn <loreunjohn@gmail.com>
2022-03-13 14:49:24 +0700

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,8 @@
-console.log('Client side javascript is loaded!')
-
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //    response.json().then((data) => {
 //       console.log(data)
 //    })
 // })
-
-
-
-
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
